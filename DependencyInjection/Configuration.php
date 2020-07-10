288586c1bf4ed788e715016912763ce7b7e6aa4b
Samuel Vicent <samuelvicent@gmail.com>
2020-07-10 13:17:26 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -30,7 +30,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('atico_spreadsheet_translator');
         $rootNode = $treeBuilder->root('atico_spreadsheet_translator');
 
         $rootNode
